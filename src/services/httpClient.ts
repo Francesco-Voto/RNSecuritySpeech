6642d54ba12f8e8529a1d04fde43a029f6c1835d
Francesco-Voto <francesco.voto@nearform.com>
2025-03-20 10:54:00 +0100

    feat: add ssl-pinning

--- src/services/httpClient.ts
+++ src/services/httpClient.ts
@@ -1,13 +1,27 @@
 import axios from 'axios';
+import {initializeSslPinning} from 'react-native-ssl-public-key-pinning';
 
-const API_ENDPOINT = 'https://rickandmortyapi.com/api';
+const API_ENDPOINT = 'rickandmortyapi.com';
 const API_TIMEOUT = 20000;
 
 const httpClient = axios.create({
-  baseURL: API_ENDPOINT,
+  baseURL: `https://${API_ENDPOINT}/api`,
   timeout: API_TIMEOUT,
   headers: {
     'Content-Type': 'application/json',
+  },
+});
+
+const publicKeyHashes = [
+  '7E+y6QfEb+IfF6tI5AJLcbBn67hVFfTFTkFpq9hPjHo=',
+  '0Bbh/jEZSKymTy3kTOhsmlHKBB32EDu1KojrP3YfV9c=',
+  'C5+lpZ7tcVwmwQIMcRtPbsQtWLABXhQzejna0wHFr8M=',
+];
+
+initializeSslPinning({
+  [API_ENDPOINT]: {
+    includeSubdomains: true,
+    publicKeyHashes,
   },
 });
 
